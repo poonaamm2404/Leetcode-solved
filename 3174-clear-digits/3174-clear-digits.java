eef1d5228233722f023ad4f94d0ae6db03229d5b
poonaamm2404 <poonaamm2404@gmail.com>
2026-07-24 11:45:00 +0530

    Time: 3 ms (41.69%), Space: 44.2 MB (23.38%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+class Solution {
+    public String clearDigits(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isDigit(ch)) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            ans.append(stack.pop());
+        }
+
+        return ans.reverse().toString();
+    }
+}
