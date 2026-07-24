f58fea595dc7953660dd0d72d9d120657a4c5a08
poonaamm2404 <poonaamm2404@gmail.com>
2026-07-24 12:20:46 +0530

    Time: 37 ms (28.41%), Space: 47 MB (65.54%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!st.isEmpty() && st.peek() == ch) {
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        while (!st.isEmpty()) {
+            ans.append(st.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
